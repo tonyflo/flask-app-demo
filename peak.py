eae9bf7d0842df10f684ce8fc6a0d27b00fefebc
Tony Teaches Tech <tony@tonyteaches.tech>
2022-10-02 15:59:05 +0000

    Using Flask template

--- peak.py
+++ peak.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 app.run(host='0.0.0.0', port=80)
